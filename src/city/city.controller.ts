2a45e25d36ef25c5adefb2b8862e3d7842d04837
Ade Sajim <a.sajim@innovattic.com>
2023-04-19 19:40:16 +0200

    Add swagger

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { CityService } from './city.service';
 import { CityDto } from './dto/city.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('cities')
+@ApiTags('City')
+@Controller('api/v1/cities')
 export class CityController {
   constructor(private cityService: CityService) {}
 
+  @ApiOperation({ summary: 'Get cities' })
   @Get()
   getCities(): CityDto[] {
     return this.cityService.getCities();
   }
 
+  @ApiOperation({ summary: 'Get cities count' })
   @Get('/count')
   getCitiesCount(): number {
     return this.cityService.getCitiesCount();
