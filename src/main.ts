167339393ee04af6b54485e365247d053a614989
Ade Sajim <a.sajim@innovattic.com>
2023-04-21 13:16:33 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ function setupSwagger(app: INestApplication) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   setupSwagger(app);
 
   await app.listen(process.env.PORT || 8080);
